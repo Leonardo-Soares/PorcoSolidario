281a88ee446498a14296583516e5dda4eff6d632
Victor Ribeiro <devjvribeiro@gmail.com>
2024-04-02 02:13:13 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
-const { primary } = require('laravel-mix/src/Mix')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './.vulmix/**/pages/*.{vue,html,js}',
-    './.vulmix/**/components/*.{vue,html,js}',
-    './pages/*.{vue,html,js}',
-    './components/*.{vue,html,js}',
+    "./app.vue",
+    "./pages/**/*.{vue,js}",
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.{vue,js}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#006CA8',
-        secondary: '#FFCD42',
-        paragraph: '#283847',
-        gray: '#94A3B8',
+        primary: "#006CA8",
+        secondary: "#FFCD42",
+        paragraph: "#283847",
+        gray: "#94A3B8",
       },
     },
   },
   plugins: [],
-}
+};
